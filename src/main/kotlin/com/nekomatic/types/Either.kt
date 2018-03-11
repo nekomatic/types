af97e6267d63d55e5068e5d15f90256a0ebb833c
nekomatic <mike@nekomatic.com>
2018-03-11 15:23:26 +0000

    Added Either

---
+++ src/main/kotlin/com/nekomatic/types/Either.kt
@@ -0,0 +1,7 @@
+package com.nekomatic.types
+
+sealed class Either<out T1 : Any, out T2 : Any> {
+    data class Left<out T1 : Any, out T2 : Any>(val value: T1) : Either<T1, T2>()
+    data class Right<out T1 : Any, out T2 : Any>(val value: T2) : Either<T1, T2>()
+}
+
