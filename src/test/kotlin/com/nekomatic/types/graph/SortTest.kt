dd0144ceaaf79ba104a277f71e71b7815aad24b7
nekomatic <mike@nekomatic.com>
2018-10-29 21:01:54 +0000

    fixed graph sort tests

--- src/test/kotlin/com/nekomatic/types/graph/SortTest.kt
+++ src/test/kotlin/com/nekomatic/types/graph/SortTest.kt
@@ -3,6 +3,7 @@ package com.nekomatic.types.graph
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertAll
 
 internal class SortTest {
 
@@ -19,19 +20,48 @@ internal class SortTest {
             .addEdge("Physics", "Game")
             .addEdge("Graphics", "Game")
             .addEdge("AI Engine", "Game")
-//            .addEdge("Game", "Logging")
+
+    fun Map<String, Int>.compare(bigger: String, smaller: String): Boolean {
+        val s = this[smaller]!!
+        val b = this[bigger]!!
+        return s < b
+    }
 
     @Test
     fun khanSort() {
         val sorted = g.khanSort()
-        val expected = listOf("Logging", "Commons", "Networking", "Math", "AI Engine", "Graphics", "Physics", "Game")
-        assertEquals(expected, sorted)
+        val dict = sorted.map { it to sorted.indexOf(it) }.toMap()
+        assertAll(
+                { assertTrue(dict.compare("Game", "AI Engine")) },
+                { assertTrue(dict.compare("Game", "Graphics")) },
+                { assertTrue(dict.compare("Game", "Physics")) },
+                { assertTrue(dict.compare("Game", "Logging")) },
+                { assertTrue(dict.compare("Game", "Networking")) },
+                { assertTrue(dict.compare("AI Engine", "Math")) },
+                { assertTrue(dict.compare("Graphics", "Math")) },
+                { assertTrue(dict.compare("Physics", "Math")) },
+                { assertTrue(dict.compare("Networking", "Logging")) },
+                { assertTrue(dict.compare("Math", "Commons")) },
+                { assertTrue(dict.compare("Physics", "Commons")) }
+        )
     }
 
     @Test
     fun dfsSort() {
         val sorted = g.dfsSort()
-        val expected = listOf("Logging", "Networking", "Commons", "Math", "AI Engine", "Graphics", "Physics", "Game")
-        assertEquals(expected, sorted)
+        val dict = sorted.map { it to sorted.indexOf(it) }.toMap()
+        assertAll(
+                { assertTrue(dict.compare("Game", "AI Engine")) },
+                { assertTrue(dict.compare("Game", "Graphics")) },
+                { assertTrue(dict.compare("Game", "Physics")) },
+                { assertTrue(dict.compare("Game", "Logging")) },
+                { assertTrue(dict.compare("Game", "Networking")) },
+                { assertTrue(dict.compare("AI Engine", "Math")) },
+                { assertTrue(dict.compare("Graphics", "Math")) },
+                { assertTrue(dict.compare("Physics", "Math")) },
+                { assertTrue(dict.compare("Networking", "Logging")) },
+                { assertTrue(dict.compare("Math", "Commons")) },
+                { assertTrue(dict.compare("Physics", "Commons")) }
+        )
     }
 }
